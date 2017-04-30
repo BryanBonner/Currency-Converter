d96e49926aff78cc5281be17f9f60d657cb1f3c6
BryanBonner <bonnerbryan@gmail.com>
2017-04-30 14:24:08 -0700

    initial files, HomeView and swipe gestures added

---
+++ CurrencyConverter/CurrencyConverter/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  CurrencyConverter
+//
+//  Created by CampusUser on 4/29/17.
+//  Copyright © 2017 CPSC411. All rights reserved.
+//
+
+import UIKit
+
+class HomeView: UITableView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
