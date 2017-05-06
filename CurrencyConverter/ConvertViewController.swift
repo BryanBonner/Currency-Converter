9981d30ea94cddeadcd4f85e8be51b856bfa7073
BryanBonner <bonnerbryan@gmail.com>
2017-05-06 15:36:28 -0700

    added Home section title - need to add foreign and allow multiple selections

--- CurrencyConverter/ConvertViewController.swift
+++ CurrencyConverter/ConvertViewController.swift
@@ -8,8 +8,34 @@
 
 import UIKit
 
-class ConvertViewController: UIViewController {
-
+class ConvertViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var list = ["Data", "Goes", "here"]
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Number of rows equal to number in the list
+        return list.count
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return cell
+    }
+    
+    // Swipe on a table view row to and select delete to remove the item from the view/list
+    // Courtesy of www.ioscreator.com/tutorials/delete-rows-table-view-ios8-swift
+    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            list.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: UITableViewRowAnimation.automatic)
+        }
+    }
+    public func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Home"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -20,16 +46,5 @@ class ConvertViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
